5c713259db7e784866d2d072997dfc15b76ee409
YaniscaGit <yanisca13@gmail.com>
2024-04-29 15:50:40 -0500

    soluciono lo que dañe

--- php/database.php
+++ php/database.php
@@ -1,9 +1,7 @@
 <?php
-<<<<<<< HEAD
 $conexion = mysqli_connect("localhost", "root", "", "useraccount");
 // initialize variables for the insert query 
-//INICIO
-=======
+//carlos daño el codigo
+
 //conection to database
 $conexion = mysqli_connect("localhost", "root", "", "useraccount");
->>>>>>> c0162c35507dafe143cdbfb365e6386d47270179
