c73d235efdb04316294b0082c662979c0972472c
YaniscaGit <yanisca13@gmail.com>
2024-04-29 16:02:21 -0500

    ya actualize todo

--- php/database.php
+++ php/database.php
@@ -1,6 +1,2 @@
 <?php
 $conexion = mysqli_connect("localhost", "root", "", "useraccount");
-// initialize variables for the insert query 
-//Cambio prueba 1
-//conection to database
-$conexion = mysqli_connect("localhost", "root", "", "useraccount");
