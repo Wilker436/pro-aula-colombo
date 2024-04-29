e15c849c00d69e35894c0eb5d8ca8178b29a5655
Lenovo <wikerpachecoperez@gmail.com>
2024-04-29 15:56:47 -0500

    probando cambios

--- php/database.php
+++ php/database.php
@@ -1,7 +1,6 @@
 <?php
 $conexion = mysqli_connect("localhost", "root", "", "useraccount");
 // initialize variables for the insert query 
-//carlos daño todo
-
+//Cambio prueba 1
 //conection to database
 $conexion = mysqli_connect("localhost", "root", "", "useraccount");
