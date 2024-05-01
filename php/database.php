93cd483137fa318245362f480087273e68e1f724
Carlos <marrugovargascarlos@gmail.com>
2024-04-30 21:01:24 -0500

    database solution

--- php/database.php
+++ php/database.php
@@ -1,8 +1,4 @@
 <?php
 $conexion = mysqli_connect("localhost", "root", "", "colombo_pa4");
 
-<<<<<<< HEAD
 //lynn y sus gatos
-=======
-//gato - gato
->>>>>>> 6bac1d804d7fb654058e987b2602276c1bfb2976
