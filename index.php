e7166312ed1b0df2e2c298d526a9e8eb9a60f5d7
YaniscaGit <yanisca13@gmail.com>
2024-05-01 19:31:11 -0500

    Nosotros completo

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
 
                 <div class="social">
                     <div><a href="#"><i class="fa-brands fa-twitter fa-2xl"></i></a></div>
-                    <div><a href="#"><i class="fa-brands fa-instagram fa-2xl"></i></a></div>
+                    <div><a href="https://www.instagram.com/senderodenomada/"><i class="fa-brands fa-instagram fa-2xl"></i></a></div>
                 </div>
 
             </div>
